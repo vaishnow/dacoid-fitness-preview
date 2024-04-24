7e784fc2a44bf4d0676edc5a9ca411f1ae3dd606
vaishnow <142155359+vaishnow@users.noreply.github.com>
2024-04-24 15:29:35 +0530

    add switch style and animations

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -1,11 +1,25 @@
+import { useId } from "react";
+import styles from "./Switch.module.css";
+
 type Props = { accentColor?: string; enabled?: boolean; className?: string };
 
 const Switch = ({ className }: Props) => {
+  const inputId = useId();
+
+  const addAnimation = () => {
+	const inputElement=document.getElementById(inputId)
+	if(inputElement){
+		inputElement.classList.add(styles.warp)
+		inputElement.removeAttribute("onchange");
+	}
+  };
 
   return (
-    <label className={`bg-[#8099FF] ${className}`}>
+    <label className={`bg-[#8099FF] ${className} ${styles.switch}`}>
       <input
+        onChange={addAnimation}
         type="checkbox"
+        id={inputId}
         className='before:bg-white after:bg-white'
       />
     </label>
